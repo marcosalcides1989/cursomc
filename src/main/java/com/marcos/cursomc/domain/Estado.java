0db538179183cdaa1fff53dcf2a81d1d7aacc930
Marcos Alcides <marcosalcides1989@gmail.com>
2022-02-21 12:00:10 -0300

    /clientes/ id disponivel para consulta

--- src/main/java/com/marcos/cursomc/domain/Estado.java
+++ src/main/java/com/marcos/cursomc/domain/Estado.java
@@ -11,6 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -20,9 +23,8 @@ public class Estado implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	
+	@JsonBackReference
 	@OneToMany(mappedBy = "estado")
-	
 	private List<Cidade> cidades = new ArrayList<>();
 	
 	
---
+++ src/main/java/com/marcos/cursomc/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.marcos.cursomc.services;
+
+import java.util.Optional;
+import java.lang.Integer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcos.cursomc.domain.Cliente;
+import com.marcos.cursomc.repositories.ClienteRepository;
+import com.marcos.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+			
+		}
+
+}
