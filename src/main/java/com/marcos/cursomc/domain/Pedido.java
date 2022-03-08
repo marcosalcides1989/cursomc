539db448b475c976cd3f5ec4a0d143f37c074fc4
Marcos Alcides <marcosalcides1989@gmail.com>
2022-03-07 23:52:02 -0300

    inserindo uma nova categoria

--- src/main/java/com/marcos/cursomc/domain/Pedido.java
+++ src/main/java/com/marcos/cursomc/domain/Pedido.java
@@ -1,10 +1,8 @@
 package com.marcos.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -19,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -32,11 +29,11 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
 	
-	@JsonManagedReference
+	
 	@OneToOne(cascade=CascadeType.ALL,mappedBy = "pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
+	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
