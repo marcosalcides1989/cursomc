b3da22b8cfae42f47100f21f2fd89323de037293
Marcos Alcides <marcosalcides1989@gmail.com>
2022-03-09 19:22:52 -0300

    paginação com parametros opcionais na requisição

--- src/main/java/com/marcos/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcos/cursomc/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.lang.Integer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcos.cursomc.domain.Categoria;
@@ -47,5 +50,9 @@ public class CategoriaService {
 	}
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
